217ab986618706d9d6753ea015d1779a54007778
Nhat <nhat@thepixelme.com>
2023-05-07 00:50:52 -0400

    hero section

---
+++ src/components/HeroSection/HeroSection.styles.tsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+import { breakpoints } from '../Breakpoint.styles'
+import { Container, zindex } from '../Shared.styles'
+
+/**
+ * Hero
+ */
+export const Hero = styled.section`
+  padding: 50px 0;
+  min-height: 80vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+/**
+ * Container
+ */
+export const HeroContainer = styled(Container)`
+  position: relative;
+  width: 100%;
+  max-width: 640px;
+
+  text-align: center;
+  color: ${({ theme }) => theme.brand1};
+
+  @media ${breakpoints.medium} {
+  }
+`
+
+/**
+ * Heading
+ */
+
+export const Heading = styled.h1`
+  margin: 0 auto 20px auto;
+  font-size: 2rem; // 32px
+
+  @media ${breakpoints.medium} {
+    font-size: 2.5rem; // 40px
+  }
+`
+
+/**
+ * SubHeading
+ */
+
+export const SubHeading = styled.h2`
+  font-size: 1rem; // 16px
+  font-weight: 400;
+  line-height: 1.5;
+
+  @media ${breakpoints.medium} {
+    font-size: 1.125rem; // 18px
+  }
+`
