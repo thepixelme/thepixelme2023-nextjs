e89b2cf652908695b51740ddf4b8afa4332a0460
Nhat <nhat@thepixelme.com>
2023-07-10 07:46:57 -0400

    escaped some special characters

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
   return (
     <Hero>
       <HeroContainer>
-        <Heading>Hi, I'm Nhat, a Front-End Developer from the USA, Earth</Heading>
+        <Heading>Hi, I&aposm Nhat, a Front-End Developer from the USA, Earth</Heading>
         <SubHeading>I bring concepts to reality in the digital realm with React, React Native, NodeJS, PHP, &amp; various APIs.</SubHeading>
       </HeroContainer>
     </Hero>
--- src/components/PortfolioSectionTemporary/PortfolioSectionTemporary.tsx
+++ src/components/PortfolioSectionTemporary/PortfolioSectionTemporary.tsx
@@ -7,7 +7,7 @@ const PortfolioSectionTemporary = () => {
       <PortfolioTemporaryContainer>
         <Heading>My Works</Heading>
         <Text>
-          Coming Soon! I'm in the process of reworking my portfolio to better showcase my skills and experience. Please check back later.
+          Coming Soon! I&aposm in the process of reworking my portfolio to better showcase my skills and experience. Please check back later.
         </Text>
       </PortfolioTemporaryContainer>
     </PortfolioTemporary>
