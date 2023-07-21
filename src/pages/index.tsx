65a48a904ed119ce050d61dad75532e7b2e54107
Nhat <nhat@thepixelme.com>
2023-07-21 12:29:16 -0400

    updated README & added Google Analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import MyStackSection from '@/components/MyStackSection/MyStackSection'
 import PortfolioSection from '@/components/PortfolioSection/PortfolioSection'
 import PortfolioSectionTemporary from '@/components/PortfolioSectionTemporary/PortfolioSectionTemporary'
 import ContactSection from '@/components/ContactSection/ContactSection'
+import Script from 'next/script'
 
 type HomePageProps = {
   toggleTheme?: () => void;
@@ -15,11 +16,24 @@ export default function Home({ toggleTheme, theme } : HomePageProps) {
   return (
     <>
       <Head>
-        <title>Home | ThePixelMe.com</title>
-        <meta name="description" content="Nhat Nguyen, Front-End Developer" />
+        <title>Nhat, Web Developer & Consultant | ThePixelMe.com</title>
+        <meta name="description" content="Nhat Nguyen, Front-End Developer from the USA with over a decade of experience" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Google Analytics  */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-L41PJJY7PE" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-L41PJJY7PE');
+        `}
+      </Script>
+      {/* ## END ## Google Analytics  */}
 
       <SiteHeader toggleTheme={toggleTheme} theme={theme} />
 
